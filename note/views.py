c41fc97707e0a9610476b6368077c4c8059c39c8
Junmo1225 <junmo20091225@hotmail.com>
2024-02-04 14:09:19 +0900

    all

---
+++ note/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Note
+from .forms import NoteForm
+# from django.contrib.auth.decorators import login_required
+# from django.db.models import Q
+
+# @login_required
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'note/note_list.html', {'notes':notes})
+
+def note_detail(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, 'note/note_detail.html', {'note': note})
+
+def create_note(request):
+    if request.method == "POST":
+        form = NoteForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form)
+            note = form.save(commit=False)
+            # note.author = request.user
+            note.save()
+            return redirect('note:note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'note/create_note.html', {'form': form})
+
+def about(request):
+    return render(request, 'note/about.html')
+
+# def login(request):
